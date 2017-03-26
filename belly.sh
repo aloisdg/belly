2c14d9b6fed730d8e3094e8f2db8707124423762
Alois <aloisdegouvello@live.fr>
2017-03-26 14:17:36 +0200

    Improve if/else line number logic

--- belly.sh
+++ belly.sh
@@ -3,6 +3,7 @@
 # http://unix.stackexchange.com/q/47407/150597
 # http://stackoverflow.com/q/192249/1248177
 # http://stackoverflow.com/q/3601515/1248177
+# http://unix.stackexchange.com/q/353922/150597
 
 while [[ $# -gt 1 ]]
 do
@@ -26,18 +27,16 @@ esac
 shift
 done
 
-if [ -z "${1+x}" ]; then
+if [[ -z "${1+x}" ]]; then
     echo "Error: Input file is missing."
     exit 1
 fi
 
-if [ "$HEAD" -gt "$TAIL" ]; then
+if [[ "$HEAD" -gt "$TAIL" ]]; then
     echo "Error: HEAD cant be greater than TAIL."
     exit 1
 fi
 
-if [ -z "${NUMBER+x}" ]; then
-    tail -n +"$HEAD" "$1" | head -n $((TAIL-HEAD+1))
-else
-    tail -n +"$HEAD" "$1" | head -n $((TAIL-HEAD+1)) | cat -n
-fi
+tail -n +"$HEAD" "$1" |
+head -n $((TAIL-HEAD+1)) |
+( if [[ -n "${NUMBER+x}" ]]; then cat -n; fi )
